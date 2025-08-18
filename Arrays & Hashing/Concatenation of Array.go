e2247dc1339f6f5a40d3815ebbca3200518676b6
muralimohans <muralimohans@yakshnatechnologies.in>
2025-08-18 17:05:09 -0400

    neetcode-1 completed

---
+++ Arrays & Hashing/Concatenation of Array.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+// Specifically, ans is the concatenation of two nums arrays.
+
+// Return the array ans.
+
+// Example 1:
+
+// Input: nums = [1,2,1]
+// Output: [1,2,1,1,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+// - ans = [1,2,1,1,2,1]
+// Example 2:
+
+// Input: nums = [1,3,2,1]
+// Output: [1,3,2,1,1,3,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+// - ans = [1,3,2,1,1,3,2,1]
+
+func getConcatenation(nums []int) []int {
+	size := len(nums)
+	result := make([]int, 2*size)
+
+	for i, num := range nums {
+		result[i], result[size+i] = num, num
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(getConcatenation([]int{1, 2, 1}))
+	fmt.Println(getConcatenation([]int{1, 3, 2, 1}))
+}
