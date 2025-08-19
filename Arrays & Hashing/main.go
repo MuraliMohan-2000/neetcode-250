ae29fd0e1309ea538dccad044c6711fbee2b7102
muralimohans <muralimohans@yakshnatechnologies.in>
2025-08-18 23:51:01 -0400

    neetcode-9 completed

--- Arrays & Hashing/main.go
+++ Arrays & Hashing/main.go
@@ -39,4 +39,15 @@ func main() {
 	fmt.Println(majorityElement([]int{3, 2, 3}))
 	fmt.Println(majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
 
+	//neetcode-9
+	myHashSet := Constructor()
+	myHashSet.Add(1)      // set = [1]
+	myHashSet.Add(2)      // set = [1, 2]
+	myHashSet.Contains(1) // return True
+	myHashSet.Contains(3) // return False, (not found)
+	myHashSet.Add(2)      // set = [1, 2]
+	myHashSet.Contains(2) // return True
+	myHashSet.Remove(2)   // set = [1]
+	myHashSet.Contains(2) // return False, (already removed)
+
 }
